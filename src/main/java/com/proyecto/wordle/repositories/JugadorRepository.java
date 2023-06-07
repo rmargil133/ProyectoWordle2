8115ee66b67bb5651df18a97786d449927658947
Ruben Mariscal <92536400+rmargil133@users.noreply.github.com>
2023-06-07 20:01:16 +0200

    Delete src/main directory

--- src/main/java/com/proyecto/wordle/repositories/JugadorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto.wordle.repositories;
-import com.proyecto.wordle.model.Jugador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-public interface JugadorRepository extends JpaRepository<Jugador, Integer>{
-    List<Jugador> findByNombre(String nombre);
-}
